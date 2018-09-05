3c5c83d922f3a89e55cacf39f417fc5ad78f8f76
Rahil <info@focustechno.in>
2018-09-05 10:41:26 +0530

    laravel crud using vue js

---
+++ database/migrations/2018_09_04_120814_add_testagain_column_to_company_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTestagainColumnToCompanyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            $table->string('testagain');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('companies', function (Blueprint $table) {
+            //
+        });
+    }
+}
